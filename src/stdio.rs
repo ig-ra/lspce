67f75b46bf06042a1921192cd7565cc78816e589
Igor Rayak <igor.rayak@gmail.com>
2025-08-02 21:46:50 +0300

    feat: clean stdio threads, no busy wait

--- src/stdio.rs
+++ src/stdio.rs
@@ -25,20 +25,12 @@ use crate::{
 const LSPCE_ID: &str = "LSPCE";
 const LSPCE_ERR: i32 = -375; // -sum([ord(c) for c in list("LSPCE")])
 
-/// Checks if thread should exit based on exit flag
-fn should_exit(exit_flag: &Arc<AtomicBool>, thread_name: &str) -> bool {
-    let exit = exit_flag.load(Ordering::Relaxed);
-    if exit {
-        Logger::info(&format!("[LSP {}] thread requested to exit", thread_name));
-    }
-    exit
-}
-
-macro_rules! bail_if_should_exit {
+macro_rules! break_if_should_exit {
     ($exit_flag:expr, $thread_name:expr) => {{
-        if should_exit($exit_flag, $thread_name) {
-            return Ok(());
+        if $exit_flag.load(Ordering::Relaxed) {
+            Logger::info(&format!("[LSP{}] - requested to exit", $thread_name));
         }
+        break;
     }};
 }
 
@@ -51,14 +43,24 @@ pub(crate) fn stdio_transport(
     let writer_thread = thread::spawn(move || {
         let mut stdin = child_stdin;
         loop {
-            bail_if_should_exit!(&exit_writer, "<");
+            break_if_should_exit!(&exit_writer, '<');
 
-            let recv_value = r_to_lsp.recv_timeout(std::time::Duration::from_millis(1));
-            match recv_value {
-                Ok(msg) => msg.write(&mut stdin),
-                Err(t) => Ok(()),
-            };
+            match r_to_lsp.recv() {
+                Ok(msg) => {
+                    if let Err(e) = msg.write(&mut stdin) {
+                        Logger::error(&format!("[LSP<] - error {}", e));
+                        exit_writer.store(true, Ordering::Relaxed);
+                        return Err(e);
+                    }
+                }
+                Err(e) => {
+                    Logger::info(&format!("[LSP<] - channel closed {}", e));
+                    exit_writer.store(true, Ordering::Relaxed);
+                    break;
+                }
+            }
         }
+        Ok(())
     });
 
     // Reader is a blocking I/O thread that reads from LSP stdio pipe.
@@ -70,13 +72,10 @@ pub(crate) fn stdio_transport(
     let (s_from_lsp, r_from_lsp) = bounded::<Message>(10);
     let reader_thread = thread::spawn(move || {
         let mut reader = std::io::BufReader::new(child_stdout);
-        let res = Ok(());
+        let mut res = Ok(());
 
         loop {
-            if exit_reader.load(Ordering::Relaxed) {
-                Logger::info(&format!("[LSP>] - requested to exit"));
-                break;
-            }
+            break_if_should_exit!(&exit_reader, '>');
 
             match Message::read(&mut reader) {
                 Ok(m) => {
@@ -101,7 +100,7 @@ pub(crate) fn stdio_transport(
         // notify dispatcher in all ways - exit flag, message and dropping channel
         exit_reader.store(true, Ordering::Relaxed);
         let _ = s_from_lsp.send(Response::new_err(LSPCE_ID, LSPCE_ERR, "").into());
-        res;
+        res
     });
 
     const MAX_STDERR_LINE_LEN: usize = 4096;
@@ -109,8 +108,9 @@ pub(crate) fn stdio_transport(
     let stderr_thread = thread::spawn(move || -> io::Result<()> {
         let mut reader = std::io::BufReader::new(child_stderr);
         let mut buffer = String::new();
+
         loop {
-            bail_if_should_exit!(&exit_stderr, "!");
+            break_if_should_exit!(&exit_stderr, '!');
 
             buffer.clear();
             match reader.read_line_limited_or_eof(&mut buffer, MAX_STDERR_LINE_LEN) {
@@ -121,13 +121,13 @@ pub(crate) fn stdio_transport(
                     Logger::error(&format!("[LSP!] - ...Truncated.. {}", &buffer.trim_end()));
                 }
                 Err(e) => {
-                    // we may signal coordinated shutdown on error here as well
-                    // but let's leave the decision to stdin/stdout threads
+                    // don't signal coordinated shutdown on error here. Leave the decision to stdin/stdout threads
                     Logger::error(&format!("[LSP!] - error: {}", e));
                     return Err(e); // Exit on unrecoverable errors including pipe close/EOF
                 }
             }
         }
+        Ok(())
     });
 
     let threads = make_io_threads(reader_thread, writer_thread, Some(stderr_thread));
